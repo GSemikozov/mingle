3e2a069ca1e1d666c35651f44d08363a76e29d44
gera <semikozovgerman@gmail.com>
2015-11-10 16:03:04 +0200

    rm toggle theme buttons, fix bugs

--- build/js/scripts.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('#grey').on('click', function() {
-       $('#main-page').removeClass('blue-theme').addClass('grey-theme');
-    });
-    $('#blue').on('click', function() {
-        $('#main-page').removeClass('grey-theme').addClass('blue-theme');
-    });
-});
